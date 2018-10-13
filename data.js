7f46703ede87a06a53ac70ab4a0b74f20cc3cdeb
Daniella Billini <Daniellabillini@gmail.com>
2018-10-13 15:26:01 -0400

    did term#1 challenge

--- data.js
+++ data.js
@@ -2,4 +2,25 @@
 let message = "Hello Daniella";
 console.log(message);
 
+let doesLikeBacon = true;
+console.log(doesLikeBacon);
+
+let doesLikeBts = false;
+console.log(doesLikeBts);
+
+let anime = "Citrus is life";
+console.log(anime);
+
+let uwu = "Noodles";
+console.log(uwu);
+
+let unique = 45;
+console.log(unique);
+
+let lordes = 37;
+console.log(lordes);
+
+let enrique = 23;
+console.log(enrique);
+
 // Type your code below!
