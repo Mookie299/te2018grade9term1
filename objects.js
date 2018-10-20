a8b61d4bb5674f7b02a05aa6b4fe0fae9670f75c
abdul <qureshi.abdul@gmail.com>
2018-10-20 15:39:10 -0400

    Character

--- objects.js
+++ objects.js
@@ -1,1 +1,29 @@
-// Write your code below!
+function character(name){
+
+this.name = name;
+this.weapon;
+this.color;
+this.relationship = true;
+this.skill;
+this.getInfo = getCharacter;
+
+}
+
+function getCharacter(){
+
+
+return this.name + " " + this.weapon + " " + this.color + " " + this.relationship;
+
+}
+
+let maximus = new character("Maximus");
+maximus.weapon = "Blaster";
+maximus.color = "Red";
+maximus.relationship = true;
+maximus.skill = "Electricity";
+
+console.log(maximus.getInfo());
+ 
+let person = {isHungry: false, firstname : "Stoneand", gender: "male", age: 30, eyecolor: 'eye'}
+
+console.log(person.firstname + person.age)
