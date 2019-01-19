6e4afb9e770d907004f65c7d63f2717f8ac7a519
Daniella Billini <Daniellabillini@gmail.com>
2019-01-19 11:24:02 -0500

    Update while.js

--- while.js
+++ while.js
@@ -1,38 +1,53 @@
 const READLINE = require("readline-sync");
 
-/**** Problem 1 ****
-Write a function that prints values from 10 to 0, counting down. Use a while
-loop!
-*/
 function countdown() {
+	 let i = 11
+ while(i>0){
+ 	i--;
+ 	console.log(i);
+ } 
+}
+ countdown();
 
+function getPassword() { 
+let word = READLINE.question("What is the password ? ");
+	if(word == 1987){
+	return console.log("congrats");
+}else{
+	while(READLINE.question("what is the password ? "));
 }
 
-/**** Problem 2 ****
-Write a function that asks the user to enter a secret password. Until the user
-enters the correct password, the program will keep asking for all eternity!
-When they finally do enter the correct password, exit the loop and print a
-congratulatory message.
-*/
-function getPassword() {
-
 }
+//getPassword();
 
-/**** Problem 3 ****
-Enhance your function above to only allow the user to try entering the correct
-password three times. If they fail three times, the program should kick them
-out and tell them they are a robot. Otherwise it should give them the same
-congratulatory message above.
-*/
 function getPasswordEnhanced() {
+let guess = READLINE.question("What is the password");
+let password = 1026
+let count = 3 
+
+	while(password != guess && count > 0){
+		console.log("wrong pass");
+		guess = READLINE.question("What is the password? ");
+		count--;
+}
+
+if(count == 0){
+	console.log("No more tries.");
+
+}else{
+	console.log("You guessed it !!");
 
 }
 
+}
+getPasswordEnhanced();
+
+
+function getPasswordFinal() {
+
+}
 /**** Problem 4 ****
 As a final enhancement, make it so when the user enters the wrong password, the
 program tells them that they are wrong, along with how many attempts they've
 made so far.
 */
-function getPasswordFinal() {
-
-}
